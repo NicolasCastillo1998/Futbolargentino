a09d0572b7056aae431937a195249a0e339f87bf
nicolascastillo <nicoocastillo19@gmail.com>
2023-07-13 23:46:01 -0300

    preguntas terminadas, ya se puede jugar

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-//arreglo que contiene las respuestas correctas
+//arreglo que contiene las respuestas correctas.
 let correctas = [3,1,2,2,3];
 
-//arreglo donde se guardan las respuetas del usuario
+//arreglo donde se guardan las respuetas del usuario.
 let opcion_elegida=[];
 
 let cantidad_correctas=0;
 
-//funcion que toma el num de pregunta y el input elegido de esa pregunta
+//funcion que toma el num de pregunta y el input elegido de esa pregunta.
 function respuesta(num_pregunta, seleccionada){
     //guardo la respuesta elegida
     opcion_elegida[num_pregunta] = seleccionada.value;
 
-    //el siguiente codigo es para poner en color blanco
-    //el fondo de los inputs para cuando elige otra opcion
-    //armo el id para seleccionar el section correspondiente
+    //el siguiente codigo es para poner en color blanco.
+    //el fondo de los inputs para cuando elige otra opcion.
+    //armo el id para seleccionar el section correspondiente.
     id="p" + num_pregunta;
 
     labels = document.getElementById(id).childNodes;
@@ -21,11 +21,11 @@ function respuesta(num_pregunta, seleccionada){
     labels[5].style.backgroundColor = "white";
     labels[7].style.backgroundColor = "white";
 
-    //doy el color a labeel seleccionado
+    //doy el color a labeel seleccionado.
     seleccionada.parentNode.style.backgroundColor = "#cec0fc";
 }
 
-//funcion que compara los arreglos para saber cuantas estuvieran correctas
+//funcion que compara los arreglos para saber cuantas estuvieran correctas.
 function corregir(){
     cantidad_correctas = 0;
     for (i=0; i < correctas.length;i++){
